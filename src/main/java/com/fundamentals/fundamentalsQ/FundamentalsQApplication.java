679cd9dbd31f69c59509e57ab6fa45214c152c00
Tenki <sebas907.l@gmail.com>
2021-12-08 17:42:15 -0500

    First Commit

---
+++ src/main/java/com/fundamentals/fundamentalsQ/FundamentalsQApplication.java
@@ -0,0 +1,13 @@
+package com.fundamentals.fundamentalsQ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundamentalsQApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundamentalsQApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fundamentals/fundamentalsQ/FundamentalsQApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fundamentals.fundamentalsQ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundamentalsQApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
